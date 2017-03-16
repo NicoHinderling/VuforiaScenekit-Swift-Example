73ce1afe02762575d43f9bb97d6f3f658dc16e20
Nicolas Hinderling <nicolas.hinderling@gmail.com>
2017-03-15 22:32:06 -0400

    now going to break scene creation functions into these two files

--- VuforiaSampleSwift/StoneAndChipsScenes.swift
+++
@@ -1,9 +0,0 @@
-//
-//  StoneAndChipsScenes.swift
-//  VuforiaSampleSwift
-//
-//  Created by Nicolas Hinderling on 3/15/17.
-//  Copyright © 2017 Yoshihiro Kato. All rights reserved.
-//
-
-import Foundation
